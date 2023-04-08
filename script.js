fdf3613fd55f070e2a99c2f3d751f8d086157b2e
yongkeunyi <129077159+yongkeunyi@users.noreply.github.com>
2023-04-09 01:07:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.getElementById("addItem").addEventListener("click", function () {
-    const newRow = document.createElement("tr");
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("addItem").addEventListener("click", function () {
+        const newRow = document.createElement("tr");
 
-    newRow.innerHTML = `
-        <td contenteditable="true">날짜 입력</td>
-        <td contenteditable="true">활동 입력</td>
-        <td contenteditable="true">장소 입력</td>
-    `;
+        newRow.innerHTML = `
+            <td contenteditable="true">날짜 입력</td>
+            <td contenteditable="true">활동 입력</td>
+            <td contenteditable="true">장소 입력</td>
+        `;
 
-    document.querySelector("#scheduleTable tbody").appendChild(newRow);
+        document.querySelector("#scheduleTable tbody").appendChild(newRow);
+    });
 });
+
